f6610836cbd13443343e77904c7def3de0922afb
jeantanzj <33819199+jeantanzj@users.noreply.github.com>
2022-01-11 15:15:48 +0800

    feat(swagger): add swagger gen

--- backend/src/auth/dto/verify-otp.dto.ts
+++ backend/src/auth/dto/verify-otp.dto.ts
@@ -1,5 +1,11 @@
+import { Length } from 'class-validator'
 import { GenerateOtpDto } from './generate-otp.dto'
 
 export class VerifyOtpDto extends GenerateOtpDto {
+  /**
+   * One time password
+   * @example 000000
+   */
+  @Length(6, 6)
   token!: string
 }
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core'
 import { NestExpressApplication } from '@nestjs/platform-express'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 
 import { AppModule } from './app.module'
 import { ConfigService } from 'config/config.service'
@@ -12,6 +13,18 @@ async function bootstrap() {
   if (['staging', 'production'].includes(environment)) {
     app.set('trust proxy', 1)
   }
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Memos')
+    .setDescription('Memos API')
+    .setVersion('1.0')
+    .build()
+  const document = SwaggerModule.createDocument(app, swaggerConfig)
+  SwaggerModule.setup('api', app, document)
+  // How to write swagger docs? Use dtos, class validators, and other decorators
+  // https://docs.nestjs.com/openapi/cli-plugin#using-the-cli-plugin
+  // https://docs.nestjs.com/openapi/decorators
+  // https://github.com/typestack/class-validator
 
   await app.listen(config.get('port'))
 }
