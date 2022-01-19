7049a280a79274f9b6d21d6c577b80769fcfb457
cheongsiuhong <cheongsiuhong@gmail.com>
2022-01-19 22:08:23 +0800

    feat(memos): add uin and uinType to create memos dto

--- backend/src/memos/dto/create-memo.dto.ts
+++ backend/src/memos/dto/create-memo.dto.ts
@@ -1,10 +1,18 @@
 import { IsNotEmpty, IsNotEmptyObject, IsOptional } from 'class-validator'
+import { UinType } from 'database/entities'
 export class CreateMemoDto {
   @IsNotEmpty()
   templateId!: number
 
   @IsOptional()
   versionId?: number
+
+  @IsNotEmpty()
+  // TODO: Create a validator for UINs?
+  uin!: string
+
+  @IsNotEmpty()
+  uinType!: UinType
 
   @IsNotEmptyObject()
   params!: Record<string, string>
