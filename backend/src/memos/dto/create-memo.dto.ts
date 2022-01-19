a4f4eceb10a5416d960bbd5a73e77f16349e72c2
cheongsiuhong <cheongsiuhong@gmail.com>
2022-01-19 21:27:46 +0800

    feat(memos): add create memo dtos

--- backend/src/memos/dto/create-memo.dto.ts
+++ backend/src/memos/dto/create-memo.dto.ts
@@ -1,2 +1,15 @@
-export class CreateMemoDto {}
-export class CreateMemoResponseDto {}
+import { IsNotEmpty, IsNotEmptyObject, IsOptional } from 'class-validator'
+export class CreateMemoDto {
+  @IsNotEmpty()
+  templateId!: number
+
+  @IsOptional()
+  versionId?: number
+
+  @IsNotEmptyObject()
+  params!: Record<string, string>
+}
+
+export class CreateMemoResponseDto {
+  slug!: string
+}
