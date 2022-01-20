77f7a8743faffe6fdda5fa138f3d365088d4e162
Daryl Chan <git@dvrylc.net>
2022-01-20 17:27:15 +0800

    feat: fetch memo from api

--- frontend/src/pages/viewer/Viewer.tsx
+++ frontend/src/pages/viewer/Viewer.tsx
@@ -1,8 +1,28 @@
+import { useQuery } from 'react-query'
 import { useParams } from 'react-router-dom'
 import { Container } from '@chakra-ui/react'
 
+import { getMemo } from '~features/memos/MemosService'
+
 export const Viewer = (): JSX.Element => {
   const { slug } = useParams()
+  const {
+    isLoading,
+    isError,
+    data: memo,
+  } = useQuery(['memo', slug], () => getMemo(slug as string), {
+    enabled: !!slug, // only fetch if slug is defined
+  })
 
-  return <Container marginTop="2em">{slug}</Container>
+  return (
+    <Container marginTop="2em">
+      <pre>
+        {isLoading
+          ? 'loading...'
+          : isError || !memo
+          ? 'dead'
+          : JSON.stringify(memo.body, null, 2)}
+      </pre>
+    </Container>
+  )
 }
