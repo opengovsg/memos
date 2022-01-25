92da9bdd7cf205f8b763178c63b7ae605271e7c8
jeantanzj <33819199+jeantanzj@users.noreply.github.com>
2022-01-25 17:45:38 +0800

    chore: support smtp (#93)

--- backend/src/mailer/mailer.service.ts
+++ backend/src/mailer/mailer.service.ts
@@ -1,31 +1,51 @@
 import { Injectable } from '@nestjs/common'
 import { Logger } from '@nestjs/common'
 import nodemailer, { SendMailOptions, Transporter } from 'nodemailer'
-import { SES } from 'aws-sdk'
+import { SESClient } from '@aws-sdk/client-ses'
 
 import { ConfigService } from '../config/config.service'
+import SMTPTransport from 'nodemailer/lib/smtp-transport'
 
 @Injectable()
 export class MailerService {
   constructor(private config: ConfigService) {}
 
-  private mailer: Pick<Transporter, 'sendMail'> = this.config.get('awsRegion')
-    ? nodemailer.createTransport({
-        SES: new SES({
+  private mailer: Pick<Transporter, 'sendMail'> = (() => {
+    if (
+      this.config.get('smtp.host') &&
+      this.config.get('smtp.user') &&
+      this.config.get('smtp.password') &&
+      this.config.get('smtp.port')
+    ) {
+      return nodemailer.createTransport({
+        host: this.config.get('smtp.host'),
+        port: this.config.get('smtp.port'),
+        auth: {
+          user: this.config.get('smtp.user'),
+          pass: this.config.get('smtp.port'),
+        },
+      } as SMTPTransport.Options)
+    }
+    if (this.config.get('awsRegion')) {
+      return nodemailer.createTransport({
+        SES: new SESClient({
           region: this.config.get('awsRegion'),
-          httpOptions: {
-            connectTimeout: 20000,
-          },
         }),
       })
-    : {
-        sendMail: (options: SendMailOptions) => {
-          Logger.log(JSON.stringify(options, null, 2))
-          return Promise.resolve(options)
-        },
-      }
+    }
+    return {
+      sendMail: (options: SendMailOptions) => {
+        options = { ...options, from: this.config.get('mailFrom') }
+        Logger.log(JSON.stringify(options, null, 2))
+        return Promise.resolve(options)
+      },
+    }
+  })()
 
   sendMail = async (mailOptions: SendMailOptions): Promise<void> => {
-    return this.mailer.sendMail(mailOptions)
+    return this.mailer.sendMail({
+      ...mailOptions,
+      from: this.config.get('mailFrom'),
+    })
   }
 }
