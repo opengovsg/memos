f86d323380a0d00c8c5b69403619dc5115e04d62
cheongsiuhong <cheongsiuhong@gmail.com>
2022-02-16 20:12:27 +0800

    feat(attachment): add Attachment component (from forms)

---
+++ frontend/src/components/Field/Attachment/AttachmentDropzone.tsx
@@ -0,0 +1,31 @@
+import { DropzoneInputProps, DropzoneState } from 'react-dropzone'
+import { BiCloudUpload } from 'react-icons/bi'
+import { chakra, Icon, Link, Text, useStyles } from '@chakra-ui/react'
+
+interface AttachmentDropzoneProps {
+  inputProps: DropzoneInputProps
+  isDragActive: DropzoneState['isDragActive']
+}
+
+export const AttachmentDropzone = ({
+  inputProps,
+  isDragActive,
+}: AttachmentDropzoneProps): JSX.Element => {
+  const styles = useStyles()
+
+  return (
+    <>
+      <chakra.input {...inputProps} data-testid={inputProps.name} />
+      <Icon aria-hidden as={BiCloudUpload} __css={styles.icon} />
+
+      {isDragActive ? (
+        <Text>Drop the file here ...</Text>
+      ) : (
+        <Text>
+          <Link isDisabled={inputProps.disabled}>Choose file</Link> or drag and
+          drop here
+        </Text>
+      )}
+    </>
+  )
+}
